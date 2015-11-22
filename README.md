178f1e5702c274c9ab99daf266ad1d8ec1f86c68
helmet33 <garrettjor@gmail.com>
2015-11-22 15:35:34 +0000

    Push for Submission, restored
    Restored data from laptop and first pre submission push

--- README.md
+++ README.md
@@ -12,21 +12,18 @@ Each region should expose the same API which in turn is accessed by our external
 ### Proposed routes and entry points
 Route | Description
 ---------|------------
- / | Returns list of implemented areas in lower case, for the scope of this project galway.
+ / | Intended to return list of implemented areas in lower case, for the scope of this project galway.
  /galway/ | Returns ALL parking from all 3 data sets
  /galway/get/ | Returns same as above.
  /galway/get/all/ | Returns same as above (more descriptive).
  /galway/get/blue/ | Returns list of blue ticket (special needs) parking spots only.
  /galway/get/display/ | Returns a list of all pay and display parking spots.
  /galway/get/carparks/ | Returns a list of car parks.
- /galway/get/blue/{param} | Returns a blue ticket (special needs) parking spot based on given parameter.
- /galway/get/display/{param} | Returns a pay and display parking spot based on given parameter.
- /galway/get/carparks/{param} | Returns a car park based on given parameter.
  /galway/post/carpark/ | Add a car park.
  /galway/post/blue/ | Add a special needs parking space.
- /galway/post/display/ | Add pay and display parking area.
- /galway/delete/{param} | Delete the parking area designated by given parameter.
- /galway/update/{param} | Update the parking space designated by given parameter.
+ /galway/post/meter/ | Add pay and display parking area.
+ /galway/del/{param} | Delete the parking area designated by given parameter.
+
 
 I intend to use update as the exposed verb to avoid confusion between put and post but intend to use put as the actual HTTP verb. All lists returned will be in JSON format. These routes maybe subject to change.
 
@@ -35,11 +32,16 @@ I intend to use update as the exposed verb to avoid confusion between put and po
 
 It is my intention to use node.js (Node) and express.js (Express) to create a public API that can be queried externally and three internal APIs that query the individual data sets. Node and Express are used as they are free and open source, light-weight and effecient. The internal APIs are to be queried through the external API. Any additional modules outside those provided by Node and Express will be documented here if required. All module will be loaded through the Node Package Manager (npm) which is a collection of open source libraries.
 
+Following testing it was found to be preferable to add the three datasets to the same database and use a type value in the data to differentiate the documents. This allowed for a single api end point.
+
+Future use will require a database for each region used which would be more manageable.
+
 ### Initial thoughts on database to use
 
 I think that this project is suited to a Couchbase&trade; database due to its native, be it limited, support for geojson. This may change if another database is more suited to the task. More then one database may be used going forward, depending on the suitability of other regions data.
 
 After testing opting to use CouchDB (See testing section below - 4th November).
+Pouchdb used to connect to local/remote couch instance.
 
 
 
@@ -53,7 +55,24 @@ The initial seed data for the project is contained in the raw_data folder and is
 * BlueBadgeParkingOpenData.geojson
 
 All data was obtained through the Irish Government [Open Data portal](http://data.gov.ie) and is freely available under its [Open Data Licence](https://data.gov.ie/licence). Licences for the individual files will be provided where available.
+Data was massaged to remove default header and add header of type: parking type.
 
+The following command were used to add the data to the database.
+```bash
+curl -d @CarParkingOpenData.geojson -H "Content-type: application/json" -X POST http://127.0.0.1:5984/galway/_bulk_docs
+
+curl -d @BlueBadgeParkingOpenData.geojson -H "Content-type: application/json" -X POST http://127.0.0.1:5984/galway/_bulk_docs
+
+curl -d @ParkingMetersOpenData.geojson -H "Content-type: application/json" -X POST http://127.0.0.1:5984/galway/_bulk_docs
+```
+These commands are also located in a text file called curlcmds.txt for ease of use. Run curl from that directory after setting up empty database.
+
+An empty database called galway was created on the localhost with the name galway.
+There is nothing to stop this been remote and it has been tested connecting to a remote database on a SUSE Linux Server (See Testing). The database was created using futon:
+
+``` bash
+http://address_of_server:port/_utils/index.html
+```
 
 ## Objective
 
@@ -74,3 +93,71 @@ Section | Implementation | Outcome | Start
 Database | Install Couchdb under Open Suse 13.2 | Testing (Implementation pending test) | 29th October
 Data | Use partial data as plain JSON (no db) | Testing (No implemenataion Planned) | 31 October
 Database | Couch import working with Diamond json | Tested (Implementing on actual data | 4 November
+Data | Data can be added to single database | Implemented (Now a single API) | 12 November
+API | Tested all endpoints | Continuous (All end points tested daily) | 21 November
+
+## Examples
+Each of the get endpoints can be used via cUrl, a modern web browser or Rest testing tool e.g Postman or REStClient. Examples include:
+
+```
+http://localhost:8000/galway/
+returns json array of all documents
+
+```
+
+To post an object the type value and properties of added item are required. The type value must be valid i.e. meter, carparks or blue. Postman was used to test all routes.
+The json array you submitted will be returned on success. A few examples:
+
+HTTP POST:
+```HTTP
+POST /galway/post/carpark HTTP/1.1
+Host: localhost:8000
+Content-Type: application/json
+Cache-Control: no-cache
+
+{
+    "type": "carparks",
+    "properties":{
+        "name": "Garretts Carpark",
+        "no_spaces": 34
+        }
+}
+```
+or as cURL:
+
+```bash
+curl -X POST -H "Content-Type: application/json" -H "Cache-Control: no-cache" -d '{
+    "type": "carparks",
+    "properties":{
+        "name": "Garretts Carpark",
+        "no_spaces": 34
+        }
+}' 'http://localhost:8000/galway/post/carpark'
+```
+
+The returned value for the above will be:
+```json
+{
+"type": "carparks",
+"properties":{
+    "name": "Garretts Carpark",
+    "no_spaces": 34
+    }
+}
+```
+
+To delete an item you pass a the documents _value. This value can be extracted from the allDocs gets or the more specific gets e.g. /get/display finding the correct documents.
+
+HTTP Delete:
+```HTTP
+DELETE /del/698f9578f6ec64893c241c0f61000fbc HTTP/1.1
+Host: 127.0.0.1:8000
+Cache-Control: no-cache
+Content-Type: multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW
+```
+
+
+or as cURL:
+```bash
+curl -X DELETE -H "Cache-Control: no-cache" -H "Postman-Token: 082354bd-1973-78ba-c2ec-6a9e3984ec0d" 'http://127.0.0.1:8000/del/698f9578f6ec64893c241c0f61000fbc'
+```
--- server.js
+++ server.js
@@ -1,63 +1,230 @@
-// Import the fs module so that we can work with the file system.
-var fs = require('fs');
-// Import express to create and configure an HTTP server and routing.
+// Load modules
+
+// couch database to connect to an sysnc
+// MUST EXIST!!!
+var url = 'http://localhost:5984/galway';
+
+// Pouch will communicate with the local couchdb
+var PouchDB = require('pouchdb');
+
+// Express middleware
 var express = require('express');
 
-// Connect to remote hosted couch instance
-//var nano = require('nano')('http://redacted:5984');
 
-// Connect to local couch instance
-var nano = require('nano')('http://localhost:5984');
+// extra modules
+var request = require('request'), querystring = require('querystring');
+var bodyParser = require('body-parser');
 
-// Create a HTTP server app.
 var app = express();
+// Connect pouch to running instance of couch
+var db = new PouchDB(url);
 
-//test Data for initialisation
-var data = [
-  'Select Datasets.'
-  , 'Combine Datasets.'
-  , 'Send Peace and love.'
-];
+// body parser for json posts
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: false })); // support encoded bodies
 
-// When a user goes to /, return a dummy response.
+// test functions for console
+
+db.info().then(function (info) {
+  console.log(info);
+})
+
+// end console tests
+
+
+//////////////////////////// Test ?????????????????
+var database = "galway";
+
+
+////////////////////////////  Start routes ///////////////////////
+
+
+// >>>>>>>>>>>>>>>>>>>>>>>>  Get Methods >>>>>>>>>>>>>>>>>>>>>>>>
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+// All docs
+// Root will return all docs as json
 app.get('/', function(req, res) {
-  res.send('Project incoming prepare for launch in coming weeks!');
+
+
+       res.send(database);
+});
+
+// galway will return all docs as json
+app.get('/galway', function(req, res) {
+
+
+       db.allDocs({
+               include_docs: true,
+               descending: false,
+       }, function(err, doc) {
+               res.send(err || doc)
+
+       });
+});
+
+// galway get will return all docs as json
+app.get('/galway/get/', function(req, res) {
+
+
+       db.allDocs({
+               include_docs: true,
+               descending: false,
+       }, function(err, doc) {
+               res.send(err || doc)
+
+       });
+});
+
+// galway get all will return all docs as json
+app.get('/galway/get/all', function(req, res) {
+
+
+       db.allDocs({
+               include_docs: true,
+               descending: false,
+       }, function(err, doc) {
+               res.send(err || doc)
+
+       });
+});
+
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+// Only Car Parks of specific type
+// Using couchDb views to return a view matching the type specified
+// Example http://localhost:port/galway/get/display returns a JSON array of
+// all carparking areas that are pay & display
+
+
+// Returns JSON list of metered parking (P&D)
+app.get('/galway/get/display', function(req,res){
+    //use view to emit matching type
+    db.query(function (doc) {
+  emit(doc.type, doc);
+}, {key: 'meter'}).then(function (result) {
+  // found docs with doc.type === 'meter'
+        res.send(result);
+});
+});
+
+// Returns JSON list of carparks
+app.get('/galway/get/carparks', function(req,res){
+    //use view to emit matching type
+    db.query(function (doc) {
+  emit(doc.type, doc);
+}, {key: 'carparks'}).then(function (result) {
+  // found docs of doc.type === 'carparks'
+        res.send(result);
+});
+});
+
+// Returns JSON list of blue ticket parking (Speclial needs)
+app.get('/galway/get/blue', function(req,res){
+    //use view to emit matching type
+    db.query(function (doc) {
+  emit(doc.type, doc);
+}, {key: 'blue'}).then(function (result) {
+  // found docs with doc.type === 'blue'
+        res.send(result);
+});
 });
 
 
-// test routing at data which returns dummy data as JSON
-app.get('/data', function(req, res) {
-    res.json(data);
-});
 
-// Select element from dummy array as indexed by parameterf
-app.get('/data/:id', function(req, res) {
-    res.json(data[req.params.id]);
-});
+// ++++++++++++++++++++++++++++++++++++++++++ Posts ++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// All posts to be sent json data with type = meter || carparks || blue
+// All posts must contain a properties, this may be empty to facilitate cahnging in future.
+// Example { "type" : "blue", "properties" : { "name"="my new thing"} }}
 
-//Holders
 
-app.get('/galway/', function(req, res) {
-  res.send('Will respond with all parking areas in galway!');
-});
 
-app.get('/galway/getParking', function(req, res) {
-  res.send('Will return all car parks in Galway!');
-});
+// Post a new meter (json in json out)
+// Must be formated as type = meter and contain properties
+app.post('/galway/post/carpark',function(req,res){
+    carpark = req.body;
+if (carpark.type == "carparks" && carpark.type.properties){
+    var carpark=req.body;
+    console.log(carpark);
+    db.post(carpark);
 
-app.get('/galway/getMeters', function(req, res) {
-  res.send('Will return all on street parking in Galway!');
-});
+    res.json(carpark);
+    res.end();
 
-app.get('/galway/getBlueTicket', function(req, res) {
-  res.send('Will return all special needs designated spaces in Galway!');
-});
 
-app.get('/galway/getAllButBlue', function(req, res) {
-  res.send('All non special needs areas in galway!');
+
+}
+
+
+
 });
 
 
-// Start the server.
+// Post a new meter (json in json out)
+// Must be formated as type = meter and contain properties
+app.post('/galway/post/blue',function(req,res){
+    carpark = req.body;
+if (carpark.type == "blue" && carpark.type.properties){
+    var blue=req.body;
+    console.log(blue);
+    db.post(blue);
+
+    res.json(blue);
+    res.end();
+
+
+
+}
+
+
+
+});
+
+// Post a new meter (json in json out)
+// Must be formated as type = meter and contain properties
+app.post('/galway/post/meter',function(req,res){
+    carpark = req.body;
+if (carpark.type == "meter" && carpark.type.properties){
+    var meter=req.body;
+    console.log(meter);
+    db.post(meter);
+
+    res.json(meter);
+    res.end();
+
+
+
+}
+
+
+
+});
+
+////////////// Delete ////////////////////////////////////////////////
+
+// Delete a document
+// Give the doc id to be deleted
+
+
+app.delete("/del/:doc_id", function(req, res){
+    doc_id = req.params.doc_id;
+    db.get(doc_id).then(function(doc){
+        //get id and rev
+        docid = doc.id;
+        rev = doc.rev;
+        // call couch db remove on found elements
+        return db.remove(doc._id, doc._rev);
+
+                        }).then(function(result){
+    console.log(result);
+    }).catch(function(err) {
+             console.log(err);
+             });//end promises
+});
+
+
+
+
+
+
+// Start the server on port 8000
 var server = app.listen(8000);
-console.log("Server running on port 8000. Check it out.");
+console.log("API parking service running on port: 8000");
